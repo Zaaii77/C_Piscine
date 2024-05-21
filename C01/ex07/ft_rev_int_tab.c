4fadbc718cdc0e62418d6c7fe79ebd6496ddf475
eva <evagg@gmail.com>
2024-05-21 23:12:28 +0200

    C01 90%

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,18 @@
+void    ft_swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void    ft_rev_int_tab(int *tab, int size)
+{
+    int i;
+    int temp;
+
+    i = 0;
+    while (i <= size / 2)
+        ft_swap(&tab[i++], &tab[size-- - 1]);
+}
