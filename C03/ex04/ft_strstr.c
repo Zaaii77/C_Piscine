a6f8374706b6b9deb413793cd864eb64c73a96d4
Zaaii <zaaiishdofus@outlook.fr>
2024-05-28 13:41:01 +0200

    strstr finished

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -1,1 +1,23 @@
-char    *ft_strstr()
+char    *ft_strstr(char *str, char *to_find)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    if (*to_find == '\0')
+        return (str);
+    while (str[i])
+    {
+        while (to_find[j] && to_find[j] == str[i])
+        {
+            j++;
+            i++;
+            if (to_find[j] == '\0')
+                return (&str[i - j]);
+        }
+        j = 0;
+        i++;
+    }
+    return (0);
+}
