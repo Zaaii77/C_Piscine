a265d5ce70c6275af414e6c18ebdc931b296711a
Zaaii <zaaiishdofus@outlook.fr>
2024-05-28 13:55:12 +0200

    C03 100%

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,29 @@
+unsigned int    ft_strlen(char *str)
+{
+    unsigned int    i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+unsigned int    ft_strlcat(char *dest, char *src, unsigned int size)
+{
+    unsigned int    dest_len;
+    unsigned int    src_len;
+    unsigned int    i;
+
+    i = 0;
+    dest_len = ft_strlen(dest);
+    src_len = ft_strlen(src);
+    if (size <= dest_len)
+        return (size + src_len);
+    while (src[i] && i < size - 1)
+    {
+        dest[dest_len + i] = src[i];
+        i++;
+    }
+    dest[dest_len + i] = '\0';
+    return (src_len + dest_len);
+}
