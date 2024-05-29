1f751a8c591c12aa56b650cf5d12ad58be250072
Zaaii <zaaiishdofus@outlook.fr>
2024-05-30 01:08:17 +0200

    correction prime

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -4,10 +4,10 @@ int ft_is_prime(int nb)
 
     i = 2;
     if (nb < 0)
-        nb = -nb;
+        return 0;
     if (nb == 0 || nb == 1)
         return (0);
-    while (i < nb)
+    while (i <= nb / i)
     {
         if (nb % i == 0)
             return (0);
--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -4,10 +4,10 @@ int ft_is_prime(int nb)
 
     i = 2;
     if (nb < 0)
-        nb = -nb;
+        return 0;
     if (nb == 0 || nb == 1)
         return (0);
-    while (i < nb)
+    while (i <= nb / i)
     {
         if (nb % i == 0)
             return (0);
@@ -18,13 +18,10 @@ int ft_is_prime(int nb)
 
 int ft_find_next_prime(int nb)
 {
-    if (nb >= 2147483647 || nb < -2147483648)
+    if (nb >= 2147483647 || nb < 0)
         return (0);
     if (ft_is_prime(nb))
         return (nb);
-    else
-        if (nb < 0)
-            ft_find_next_prime(nb - 1);
-        else    
-            ft_find_next_prime(nb + 1);
+    else   
+        ft_find_next_prime(nb + 1);
 }
